86236aef973a5c4ac0bd87d11b2827ab87612a8a
Alex Gisi <apgisi11@gmail.com>
2025-01-15 10:42:46 -0600

    Implement LQR preview control
    * Start H-infinity formulation, realized it needs to be added to
    python-control

--- preview_control/controller.py
+++ preview_control/controller.py
@@ -3,9 +3,10 @@ References
 [1] "A tutorial on preview control systems", Tabaka (2003).
 [2] "hinfsyn: Compute H-infinity optimal controller", Mathworks. 
 """
-
+import scipy
 import numpy as np
 import control as ct
+import scipy.linalg
 
 
 def get_design_system(Ap, Bp, Cp, Dp, Q, R):
@@ -91,32 +92,25 @@ def design_system_lqr(A, B, Q, R):
 
 
 def get_preview_system(A, B, C, D, E, h):
-    """Augment the provided system with the preview vectors.
-    See 3.2 section [1].
-
-    :param A: (n,n)
-    :param B: (n,m)
-    :param C: (k,n)
-    :param D: (k,m)
-    :param E: (k,1)
-    :param h: number of preview steps
-    :return: _description_
+    """Augment the design system with `h` preview vectors. See 3.2 section [1].
+    In this function, k is the reference dimension.
     """
-    n = A.shape[0]  # no. states
-    m = B.shape[1]  # no. inputs
-    k = C.shape[0]  # no. measurements
+    n = A.shape[0]  # state dimension
+    m = B.shape[1]  # input dimension
+    k = E.shape[1]  # disturbance dimension
     r = k*(h+1)     # length of preview vector
+    n_underlying = n - k
 
     Ad = np.block([
         [np.eye(k) if i==j+1 else np.zeros(shape=(k, k)) for i in range(h+1)] for j in range(h+1)
     ])
     Bd = np.block([
-        [np.eye(k)] if i == h else np.zeros(shape=(k, k)) for i in range(h+1)
+        [np.eye(k)] if i == h else [np.zeros(shape=(k, k))] for i in range(h+1)
     ])
 
     F = np.block([
         [A, E, np.zeros(shape=(n, k*h))],
-        [np.zeros(shape=(r, r)), Ad],
+        [np.zeros(shape=(r, n)), Ad],
     ])
     G = np.block([
         [B],
@@ -126,38 +120,87 @@ def get_preview_system(A, B, C, D, E, h):
         [np.zeros(shape=(n, k))],
         [Bd],
     ])
-    H = np.block([
-        [C],
-        [np.zeros(shape=(k, r))]
-    ])
+    H = np.block([C, np.zeros(shape=(k+m, r))])
 
     assert Ad.shape == (r, r)
     assert Bd.shape == (r, k)
-    assert F.shape == (n + r, n + r)
-    assert G.shape == (n + r, m)
-    assert L.shape == (n + r, k)
-    assert H.shape == (k, n + r)
-    assert D.shape == (k, m)
+    assert F.shape == (n+r, n+r)
+    assert G.shape == (n+r, m)
+    assert L.shape == (n+r, k)
+    assert H.shape == (k+m, k+n_underlying+r)
+    assert D.shape == (k+m, m)
 
     return F, G, L, H, D
 
 
-def preview_system_h_infinity(F, G, L, H, D, zeta_dim, ref_dim, u_dim):
+def design_system_lqr_preview(A, B, E, Q, R, h):
+    n = A.shape[0]  # state dimension
+    m = B.shape[1]  # input dimension
+    k = E.shape[1]  # disturbance/error dimension
+    r = k*(h+1)     # length of preview vector
+    n_underlying = n - k
+
+    Ad = np.block([
+        [np.eye(k) if i==j+1 else np.zeros(shape=(k, k)) for i in range(h+1)] for j in range(h+1)
+    ])
+    F = np.block([
+        [A, E, np.zeros(shape=(n, k*h))],
+        [np.zeros(shape=(r, n)), Ad],
+    ])
+    G = np.block([
+        [B],
+        [np.zeros(shape=(r, m))],
+    ])
+
+    Qd = scipy.linalg.block_diag(
+        np.zeros(shape=(n_underlying, n_underlying)),
+        Q,
+        np.zeros(shape=(r, r))
+    )
+    Rd = R
+
+    assert Qd.shape == (n_underlying+k+r, n_underlying+k+r)
+    assert Rd.shape == (m, m)
+
+    K, S, E = ct.dlqr(F, G, Qd, Rd)
+    K_x = K[:, :n_underlying]
+    K_e = K[:, n_underlying:n_underlying+k]
+    K_r = K[:, n_underlying+k:]
+
+    assert K_x.shape == (1, n_underlying)
+    assert K_e.shape == (1, k)
+    assert K_r.shape == (1, h+1)
+
+    return K_x, K_e, K_r
+
+
+def preview_system_h_infinity(F, G, L, H, D):
+    """todo: hinfsyn not supported yet in python-control
+    """
+    zeta_dim = F.shape[0]
+    ref_dim = L.shape[1]
+    u_dim = D.shape[1]
+
     assert H.shape[0] == D.shape[0]
+    assert H.shape[1] == zeta_dim
 
     P11 = F
     P12 = np.block([L, G])
-    P21 = np.block([[H, np.eye(zeta_dim)]])
+    P21 = np.block([[H], [np.eye(zeta_dim)]])
     P22 = np.block([
         [np.zeros(shape=(H.shape[0], ref_dim)), D],
         [np.zeros(shape=(zeta_dim, ref_dim)), np.zeros(shape=(zeta_dim, u_dim))]
     ])
-    P = ct.ss(P11, P12, P21, P22)
+    P = ct.ss(P11, P12, P21, P22, dt=True)
 
     K, CL, gam, rcond = ct.hinfsyn(P, nmeas=zeta_dim, ncon=u_dim)
     K_state = K[:zeta_dim]
-    K_e = K[zeta_dim:2*zeta_dim]
-    K_r = K[2*zeta_dim:]
+    K_e = K[zeta_dim:zeta_dim+ref_dim]
+    K_r = K[zeta_dim+ref_dim:]
+
+    assert K_state.shape[1] == zeta_dim
+    assert K_r.shape[1] == ref_dim
+    assert K_e.shape[1] == ref_dim
 
     return K_state, K_e, K_r
 
@@ -180,14 +223,42 @@ class LQRController:
         self.e_accum += error
 
 
-class PreviewController:
-    def __init__(self, Ap, Bp, Cp, Dp, Q, R):
-        self.e_accum = 0
+class LQRPreviewController:
+    def __init__(self, Ap, Bp, Cp, Dp, Q, R, h):
+        self.h = h
+        C = ct.ctrb(Ap, Bp)
+        if C.shape[0] != np.linalg.matrix_rank(C):
+            raise ValueError("system not controllable")
+
+        self.e_accum = np.zeros(shape=(Q.shape[0], 1))
 
         A, B, C, D, E = get_design_system(Ap, Bp, Cp, Dp, Q, R)
+        self.K_x, self.K_e, self.K_r = design_system_lqr_preview(A, B, E, Q, R, h)
+        
+    def control(self, x, rs):
+        u = self.K_x @ x + self.K_e @ self.e_accum + self.K_r @ rs
+        return -u
 
-    def control(self, x):
-        pass
+    def update(self, error):
+        self.e_accum += error
+
+
+class PreviewController:
+    def __init__(self, Ap, Bp, Cp, Dp, Q, R, h):
+        C = ct.ctrb(Ap, Bp)
+        if C.shape[0] != np.linalg.matrix_rank(C):
+            raise ValueError("system not controllable")
+        
+        self.h = h
+        self.e_accum = 0
+
+        sys = get_design_system(Ap, Bp, Cp, Dp, Q, R)
+        preview_sys = get_preview_system(*sys, h)
+        self.K_state, self.K_e, self.K_r = preview_system_h_infinity(*preview_sys)
+
+    def control(self, x, rs):
+        u = self.K_state @ x + self.K_e @ self.e_accum + sum([ki @ ri for ki, ri in zip(self.K_r, rs)])
+        return -u
 
     def update(self, error):
         self.e_accum += error
--- preview_control/test.py
+++ preview_control/test.py
@@ -23,33 +23,62 @@ Cp = np.array([[0.0, 1.0, 0.0, 0.0]])
 Dp = np.array([[0.0]])
 
 # Use a step reference
-times = range(80)
-rs = [np.zeros(shape=(Cp.shape[0], 1)) if t < 5 else np.ones(shape=(Cp.shape[0], 1)) for t in times]
+ts = range(80)
+rs = [np.zeros(shape=(Cp.shape[0], 1)) if t < 25 else np.ones(shape=(Cp.shape[0], 1)) for t in ts]
+get_ref = lambda i, h: np.array([rs[j] if j<len(rs) else rs[-1] for j in range(i, i+h+1)]).reshape(-1, 1)
 
-# Synthesize lqr controller
+# Define control objective function
 Q = np.array([[20.0]])
 R = np.array([[1.0]])
-lqr = controller.LQRController(Ap, Bp, Cp, Dp, Q, R)
 
-# Simulate response
-x = np.zeros(shape=(Ap.shape[0], 1))
-xs = [x]
-ys = [Cp @ x]
-for t in times[:-1]:
-    u = lqr.control(x)
-    x = Ap @ x + Bp @ u
-    y = Cp @ x + Dp @ u
-    e = rs[t] - y
-    lqr.update(e)
-    xs.append(x)
-    ys.append(y)
 
-# Assume scalar output, reference
-ys = [float(y.item()) for y in ys]
+def simulate(controller, times):
+    n_preview = controller.h if hasattr(controller, 'h') else None 
+
+    x = np.zeros(shape=(Ap.shape[0], 1))
+    xs = [x]
+    ys = [Cp @ x]
+    us = []
+    for t in times[:-1]:
+        u = controller.control(x, rs=get_ref(t, n_preview)) if n_preview else controller.control(x)
+        x = Ap @ x + Bp @ u
+        y = Cp @ x + Dp @ u
+        e = rs[t] - y
+        controller.update(e)
+        xs.append(x)
+        ys.append(y)
+        us.append(u)
+
+    # Assume scalar inputs, measurements
+    ys = [float(y.item()) for y in ys]
+    us = [float(u.item()) for u in us]
+
+    return xs, ys, us
+
+trials = {
+    0: simulate(controller.LQRController(Ap, Bp, Cp, Dp, Q, R), ts),
+    5: simulate(controller.LQRPreviewController(Ap, Bp, Cp, Dp, Q, R, h=5), ts),
+    10: simulate(controller.LQRPreviewController(Ap, Bp, Cp, Dp, Q, R, h=10), ts),
+    15: simulate(controller.LQRPreviewController(Ap, Bp, Cp, Dp, Q, R, h=15), ts),
+    20: simulate(controller.LQRPreviewController(Ap, Bp, Cp, Dp, Q, R, h=20), ts),
+}
+
 rs = [float(r.item()) for r in rs]
 
-# Plot result
-plt.plot(times, ys, label='y')
-plt.plot(times, rs, 'k--', label='r')
-plt.legend()
+
+fig, axs = plt.subplots(2, 1)
+for h, res in trials.items():
+    ys = res[1]
+    us = res[2]
+
+    axs[0].plot(ts, ys, label=f'y_{h}')
+    axs[0].legend(loc='center left', bbox_to_anchor=(-0.3, 0.5))  # Move legend to the far left
+    axs[0].set_title('output')
+
+    axs[1].plot(ts[:-1], us, label=f'u_{h}')
+    axs[1].legend(loc='center left', bbox_to_anchor=(-0.3, 0.5))  # Move legend to the far left
+    axs[1].set_title('control input')
+axs[0].plot(ts, rs, 'k--', label='r')
+
+plt.tight_layout()
 plt.show()
